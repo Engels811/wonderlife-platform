ecdcbc8e58d48bd71121ef477a4a8541997e3ddc
Engels811 <54458787+Engels811@users.noreply.github.com>
2025-11-19 13:54:31 +0100

    Update page.tsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,7 +2,7 @@ export default function UsersPage() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-purple-400">Users</h1>
-      <p className="opacity-70 mt-2">Manage all users in your system.</p>
+      <p className="opacity-70 mt-2">User management.</p>
     </div>
   );
 }
