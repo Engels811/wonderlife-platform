18225d197a7a1df4e2a51b28718dd3198ef50df4
Engels811 <54458787+Engels811@users.noreply.github.com>
2025-11-19 13:55:26 +0100

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,8 @@
-export default function Login() {
+export default function LoginPage() {
   return (
-    <div className='p-10 text-white'>
-      <h1 className='text-3xl font-bold'>Login Bereich</h1>
+    <div>
+      <h1 className="text-3xl font-bold text-purple-400">Login</h1>
+      <p className="opacity-70 mt-2">Sign in to your WonderLife account.</p>
     </div>
   );
 }
